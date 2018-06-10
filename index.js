431193c134db7c99cce8f63e5b66cee77e495211
Ryan Davey <ryancdavey@icloud.com>
2018-06-10 21:04:22 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ function sayHiToGrandma(string){
   if (string === "I love you, Grandma."){
     return "I love you, too.";
   }
-  if string === {
+  if string === string.toUpperCase() {
     
   }
 }
